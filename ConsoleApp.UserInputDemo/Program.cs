7071c5dc9000b4fad74f0e8812a46ae5b5002531
bassel <basselhesham04@gmail.com>
2024-08-29 17:07:02 +0300

    Added first/last names.

--- ConsoleApp.UserInputDemo/Program.cs
+++ ConsoleApp.UserInputDemo/Program.cs
@@ -1,14 +1,24 @@
-﻿string name = string.Empty;
+﻿string firstName = string.Empty;
+string lastName = string.Empty;
 int age = 0;
+int retirementAge = 65;
 
-Console.WriteLine("Please enter your name");
-name = Console.ReadLine();
+Console.WriteLine("Please enter your first name");
+firstName = Console.ReadLine();
+
+Console.WriteLine("Please enter your last name");
+lastName = Console.ReadLine();
 
 Console.WriteLine("Please enter your age");
 age = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine($"Full Name: {name}");
- 
+int workingYearsRemaining = retirementAge - age;
+
+Console.WriteLine($"Full Name: {firstName} {lastName}");
+Console.WriteLine($"Age: {age}");
+Console.WriteLine($"Working years remaining: {workingYearsRemaining}");
 
 
- 
+
+
+
