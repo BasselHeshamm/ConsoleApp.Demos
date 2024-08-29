96f07a5428a7305ff68b47a93b00669f838c2ef1
bassel <basselhesham04@gmail.com>
2024-08-29 17:01:33 +0300

    Add project files.

---
+++ ConsoleApp.UserInputDemo/Program.cs
@@ -0,0 +1,14 @@
+﻿string name = string.Empty;
+int age = 0;
+
+Console.WriteLine("Please enter your name");
+name = Console.ReadLine();
+
+Console.WriteLine("Please enter your age");
+age = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Full Name: {name}");
+ 
+
+
+ 
